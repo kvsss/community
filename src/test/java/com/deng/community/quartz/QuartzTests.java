74467371acc6f2e5b804b92a7ba2e3c62ad4cb27
deng <912068332@qq.com>
2022-07-24 00:32:15 +0800

    quartz的使用

---
+++ src/test/java/com/deng/community/quartz/QuartzTests.java
@@ -0,0 +1,36 @@
+package com.deng.community.quartz;
+
+import com.deng.community.CommunityApplication;
+
+import org.junit.jupiter.api.Test;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class QuartzTests {
+
+    @Autowired
+    private Scheduler scheduler;
+    @Test
+    public void t1() throws InterruptedException {
+      Thread.sleep(100000);
+    }
+
+
+    @Test
+    public void testDeleteJob() {
+        try {
+            boolean result = scheduler.deleteJob(new JobKey("alphaJob", "alphaJobGroup"));
+            System.out.println(result);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
